176864820727f9026a140b3be37738e87c6d811d
GitHub Action <action@github.com>
2022-02-21 07:46:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/lglxb/intellijtemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/lglxb/intellijtemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.lglxb.intellijtemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lglxb.intellijtemplate.MyBundle
 
 class MyApplicationService {
 
